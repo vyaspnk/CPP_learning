545740734bbfb7bae85932c82a7955ba064003a0
vyaspnk <vyaspnk@gmail.com>
2017-02-14 20:07:41 -0800

    more implicit constructors assignment operators and destructors.

--- Constructors1.cpp
+++ Constructors1.cpp
@@ -11,6 +11,9 @@ public :
     int getValue() {
         return a;
     }
+    void setValue(int a) {
+        this->a = a;
+    }
 };
 
 int main() {
@@ -30,5 +33,13 @@ int main() {
 	X x_obj(111);
 	X y_obj(x_obj);
 	cout << "x_obj = " << x_obj.getValue() << " y_obj = " << y_obj.getValue() << endl;
+
+	cout << "Copy Constructor for Objects points simply calls copy constructors on pointers" << endl;
+	X *xp_obj = new X(123);
+	X *yp_obj(xp_obj);
+	cout << "*xp_obj = " << xp_obj->getValue()<< endl;
+	xp_obj->setValue(345);
+	cout << "*xp_obj = " << xp_obj->getValue();
+	cout << " y_obj = " << yp_obj->getValue() << endl;
 	return 0;
 }
--- Constructors2.cpp
+++ Constructors2.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
+class tmp{ //to show implicit desctructors.
+    
+public :
+    tmp() {}
+    ~tmp() {cout << "Tmp Destructor called" << endl;}
+};
 class X {
 private :
     int a;
     int b;
+    tmp t;
 public :
     X() { a = 0; }
     X(int a,int b) { this->a = a; this->b = b;}
-    ~X(){};
+    //X(X &s) {}
+    //~X() { cout << "Destructor called" << endl;}
     
     int getValuea() {
         return a;
@@ -24,17 +32,18 @@ int main() {
 	int x=10;
 	int y = x;	
 	cout << "x = " << x << " y = " << y << endl;
-	
+	cout << "Implicit Destructor for base types does nothing  " << endl;
 	
 	cout << "Implicit Assignment Operator for pointer types copies the pointer values(not the objects itself) " << endl;
 	int *xp = new int(5);
 	int *yp = xp;
 	cout << "*xp = " << *xp << " *yp = " << *yp << endl;
-	
+	cout << "Implicit Destructor for pointer types does nothing(no memory is deallocated)  " << endl;
+
 	cout << "Implicit Assignment for Objects simply calls Assignment Operator of the class" << endl;
 	X x_obj(111,222);
 	X y_obj = x_obj;
 	cout << "x_obj = " << x_obj.getValuea() << " " << x_obj.getValueb() << " y_obj = " << y_obj.getValuea() << " " << y_obj.getValueb() << endl;
-
+	cout << "For Object type implicit Destructor is called first and then memberwise destructors are called.  " << endl;
 	return 0;
 }
